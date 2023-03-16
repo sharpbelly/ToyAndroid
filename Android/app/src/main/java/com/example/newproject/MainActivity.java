355fa8e7c664a7066f1a912c94a08423f641be70
신주원 <90017438+sharpbelly@users.noreply.github.com>
2023-03-16 21:04:55 +0900

    안드로이드 프로젝트
    안드로이드 프로젝트

---
+++ Android/app/src/main/java/com/example/newproject/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.newproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 테스트중입니다.
+    }
+}
